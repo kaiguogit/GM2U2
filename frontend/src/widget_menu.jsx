1695d007557a1aff61880579170d8da9dd060012
kaiguogit <kaiguo1988@gmail.com>
2016-09-01 13:34:23 -0700

    fix

---
+++ frontend/src/widget_menu.jsx
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+
+class WidgetMenu extends Component {
+  render() {
+    return (
+      <div className="col s3">
+        <h1>widgets</h1>
+      </div>
+    );
+  }
+}
+export default WidgetMenu;
