4737ad4fd21b5edf85187201efdf5af245e89321
kaiguogit <kaiguo1988@gmail.com>
2016-09-01 13:34:23 -0700

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
 import React, {Component} from 'react';
+import Topbar from "./topbar.jsx";
+import WidgetMenu from "./widget_menu.jsx";
+import Board from "./board.jsx";
 
 class App extends Component {
   render() {
     return (
-      <h1>Hello React :)</h1>
+      <div>
+        <Topbar />
+        <div className="row">
+          <WidgetMenu />
+          <Board />
+        </div>
+      </div>
     );
   }
 }
---
+++ src/topbar.jsx
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class Topbar extends Component {
+  render() {
+    return (
+      <nav>
+        <div className="nav-wrapper">
+          <a href="#" className="brand-logo">Logo</a>
+        </div>
+      </nav>
+    );
+  }
+}
+export default Topbar;
