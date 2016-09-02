8213077663d6d241ec4b79d92285d2196a47e0cb
kaiguogit <kaiguo1988@gmail.com>
2016-09-01 18:21:20 -0700

    before switching to squalize

---
+++ frontend/src/widget.jsx
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+
+class Widget extends Component {
+  render() {
+    return (
+      <i className="large material-icons">insert_chart</i>
+    );
+  }
+}
+export default Widget;
