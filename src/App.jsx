e8079f2e7e8a30269f30a9a18a2d9e4f9ed4bad7
KennethQiu <kennethqiujunhao@gmail.com>
2016-09-02 18:01:39 -0700

    added active playlist to body and footer

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,30 @@ import React, {Component} from 'react';
 import Navbar from "./navbar.jsx";
 import SidebarLeft from "./sidebar_left.jsx";
 import SidebarRight from "./sidebar_right.jsx";
-
+import ActivePlaylist from './active_playlist.jsx';
+import Footer from './footer.jsx';
 
 
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {uiState: { leftSidebarOpen: false, rightSidebarOpen: false }}
+    this.state = {uiState: { sidebarLeftOpen: false, sidebarRightOpen: false }}
   }
 
-  toggleLeftSidebar() {
+  toggleSidebarLeft() {
     this.setState({
       uiState: {
-        leftSidebarOpen: !this.state.uiState.leftSidebarOpen,
-        rightSidebarOpen: false,
+        sidebarLeftOpen: !this.state.uiState.sidebarLeftOpen,
+        sidebarRightOpen: false,
+      }
+    })
+  }
+
+  toggleSidebarRight() {
+    this.setState({
+      uiState: {
+        sidebarRightOpen: !this.state.uiState.sidebarRightOpen,
+        sidebarLeftOpen: false,
       }
     })
   }
@@ -31,16 +41,23 @@ class App extends Component {
   render() {
     return (
       <div>
-        <Navbar toggleLeftSidebar={this.toggleLeftSidebar.bind(this)} />
+        {/* Navbar */}        
+        <Navbar toggleSidebarLeft={this.toggleSidebarLeft.bind(this)} toggleSidebarRight={this.toggleSidebarRight.bind(this)} />
         <div className="row">
-          <SidebarLeft open={this.state.uiState.leftSidebarOpen} />
+          {/* sidebar left - widgets */}
+          <SidebarLeft open={this.state.uiState.sidebarLeftOpen} />
+          {/* sidebar right - playlists */}
           { !this.state.playlists &&
             <div><h1>...loadding</h1></div>
           }
           { this.state.playlists &&
-            <SidebarRight playlists={this.state.playlists}/>
+          <SidebarRight open={this.state.uiState.sidebarRightOpen} playlists={this.state.playlists}/>
           }
+          {/* centered content - active playlist */}
+          <ActivePlaylist/>
         </div>
+        {/* Footer */}
+        <Footer/>
       </div>
     );
   }
--- src/sidebar_right.jsx
+++ src/sidebar_right.jsx
@@ -4,7 +4,7 @@ import Playlist from './playlist.jsx';
 class SidebarRight extends Component {
   render() {
     return (
-      <div id="slider_playlists">    
+      <div id="sidebar_right" className={this.props.open ? '' : 'hidden'}>    
         {
           this.props.playlists.map(function(playlist){
             return <Playlist playlist={playlist} key={playlist.id}/>
