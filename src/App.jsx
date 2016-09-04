1ce3d688728a16ae645cfa4758368c76dc151997
kaiguogit <git@kaiguo.ca>
2016-09-03 18:59:34 -0700

    add google auth

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,17 @@ import SidebarLeft from "./sidebar_left.jsx";
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {uiState: { leftSidebarOpen: false, rightSidebarOpen: false }}
+    this.state = {
+      uiState: { 
+        leftSidebarOpen: false, 
+        rightSidebarOpen: false 
+      },
+      username: window.localStorage.username
+    }
+  }
+
+  getChildContext() {
+    return {username: this.state.username};
   }
 
   toggleLeftSidebar() {
@@ -14,22 +24,44 @@ class App extends Component {
       uiState: {
         leftSidebarOpen: !this.state.uiState.leftSidebarOpen,
         rightSidebarOpen: false,
-      }
+      },
+      username: this.state.username
+    })
+  }
+
+  loggedIn(username){
+    this.setState({
+      uiState: this.state.uiState,
+      username: username
+    }, function(){
+      console.log("App\s state is updated. username is", this.state.username);
+      // console.log("context object is", getChildContext())
     })
   }
 
   componentDidMount() {
     console.log('App mounted');
-    $.getJSON("http://localhost:3000/api/playlists")
-    .then(function(playlists) {
-      console.log("Playlists is", playlists);
-      this.setState({playlists: playlists})
-    }.bind(this));
+    
+    if(this.state.username){
+      console.log("Sending Ajax");
+      $.ajax({
+        url: "http://localhost:3000/api/playlists",
+        method: "get",
+        headers: {
+        'Authorization':  "Bearer " + window.localStorage.token
+        }
+      })
+      .then(function(playlists) {
+        console.log("Playlists is", playlists);
+        this.setState({playlists: playlists})
+      }.bind(this));
+    }
+
   };
   render() {
     return (
       <div>
-        <Navbar toggleLeftSidebar={this.toggleLeftSidebar.bind(this)} />
+        <Navbar toggleLeftSidebar={this.toggleLeftSidebar.bind(this)} loggedIn={this.loggedIn.bind(this)} />
         <div className="row">
           <SidebarLeft open={this.state.uiState.leftSidebarOpen} />
           
@@ -38,4 +70,8 @@ class App extends Component {
     );
   }
 }
+
+App.childContextTypes = {
+  username: React.PropTypes.string
+};
 export default App;
