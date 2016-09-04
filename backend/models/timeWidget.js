16f8e009a759252b06a3d7710bdd656d0a22b1fb
kaiguogit <git@kaiguo.ca>
2016-09-04 00:00:45 -0700

    created timewidget model

--- backend/models/timeWidget.js
+++ backend/models/timeWidget.js
@@ -1,21 +1,17 @@
 "use strict"
 
 module.exports = function(sequelize, DataTypes) {
-    var Playlist = sequelize.define("playlist", {
+    var TimeWidget = sequelize.define("timeWidget", {
         name: {
             type: DataTypes.STRING,
             allowNull: false
-        },
-        widgets:{
-          type: DataTypes.JSON,
         }
     },{
       classMethods: {
         associate: function(models) {
-          Playlist.belongsTo(models.user);
-          Playlist.hasMany(models.timeWidget);
+          TimeWidget.belongsTo(models.playlist);
         }
       }
     });
-    return Playlist;
+    return TimeWidget;
 }
