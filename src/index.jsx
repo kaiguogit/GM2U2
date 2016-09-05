0b73ba53f3c2911d93a09c3dbe0e1fef3663a9d0
KennethQiu <kennethqiujunhao@gmail.com>
2016-09-04 21:15:44 -0700

    mui theme success

--- src/index.jsx
+++ src/index.jsx
@@ -7,9 +7,37 @@ import React from 'react';
 import { render } from 'react-dom';
 import { Router, browserHistory } from 'react-router';
 import routes from './routes';
+// MUI realted 
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import {cyan500} from 'material-ui/styles/colors';
+
+// MUI requied
+injectTapEventPlugin();
+
+// testing custom theme
+// const customTheme = getMuiTheme({
+//   palette: {
+//     textColor: cyan500,
+//   },
+//   appBar: {
+//     height: 80,
+//   },
+// });
+
+// render(
+//   <MuiThemeProvider muiTheme={getMuiTheme(darkBaseTheme)}>
+//     <Router routes={routes} history={browserHistory}/>
+//   </MuiThemeProvider>
+//   ,document.getElementById('react-root')
+// )
 
 
 render(
+  <MuiThemeProvider >
     <Router routes={routes} history={browserHistory}/>
-    ,document.getElementById('react-root')
+  </MuiThemeProvider>
+  ,document.getElementById('react-root')
 )
