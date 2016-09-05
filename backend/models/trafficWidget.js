dee024457ee43920c544dfa9d524f047673771d3
kaiguogit <git@kaiguo.ca>
2016-09-05 11:43:21 -0700

    added weather and traffic widget routes

--- backend/models/trafficWidget.js
+++ backend/models/trafficWidget.js
@@ -4,37 +4,6 @@ var removeFromPlaylistArray = require('./widget.js').removeFromPlaylistArray;
 
 module.exports = function(sequelize, DataTypes) {
 
-  //AfterCreate
-  var addToPlaylistArray = function(trafficWidget, options) {
-    console.log("!!!!!adding to playlist after create");
-    this.associations.playlist.target.findById(trafficWidget.playlistId)
-    .then(function(playlist){
-      var widgets = playlist.dataValues.widgets
-      widgets.push(trafficWidget);
-      playlist.updateAttributes({widgets: widgets});
-    });
-  }
-
-  //After destroy 
-  var removeFromPlaylistArray = function(trafficWidget, option) {
-    // console.log("In After Destroy trafficWidget is", trafficWidget);
-    this.associations.playlist.target.findById(trafficWidget.playlistId)
-    .then(function(playlist){
-      // console.log("Before splice, widgets is", widgets);
-
-      //Remove the widget from array
-      var widgets = playlist.dataValues.widgets;
-      widgets.forEach(function(widget, index){
-        if(widget.id === trafficWidget.id){
-          widgets.splice(index, 1);
-        }
-      });
-      // console.log("After splice, widgets is", widgets);
-      playlist.updateAttributes({widgets: widgets});
-    });
-  }
-
-
   var TrafficWidget = sequelize.define("trafficWidget", {
       id: { 
         type: DataTypes.UUID, 
