e7357dea27928548d1dd46a09e20c5f7e8d1f9e1
kaiguogit <git@kaiguo.ca>
2016-09-06 00:51:03 -0700

    styling widget card

--- frontend/src/widget_icon.jsx
+++ frontend/src/widget_icon.jsx
@@ -20,7 +20,7 @@ function collect(connect, monitor) {
 class WidgetIcon extends Component {
   componentDidMount() {
     const img = new Image();
-    img.src = './src/Maps-preview.jpg'
+    img.src = './public/Maps-preview.jpg'
     // img.onload = () => this.props.connectDragPreview(img);
   }
   render() {
--- frontend/src/widgets/TimeWidget.jsx
+++ frontend/src/widgets/TimeWidget.jsx
@@ -6,9 +6,43 @@ import {Card, CardActions, CardHeader, CardMedia, CardTitle, CardText} from 'mat
 import FlatButton from 'material-ui/FlatButton';
 import Toggle from 'material-ui/Toggle';
 
+import AppBar from 'material-ui/AppBar';
+import IconButton from 'material-ui/IconButton';
+import IconMenu from 'material-ui/IconMenu';
+import MoreVertIcon from 'material-ui/svg-icons/navigation/more-vert';
+import NavigationClose from 'material-ui/svg-icons/navigation/close';
+
+import FontIcon from 'material-ui/FontIcon';
+import NavigationExpandMoreIcon from 'material-ui/svg-icons/navigation/expand-more';
+import MenuItem from 'material-ui/MenuItem';
+import DropDownMenu from 'material-ui/DropDownMenu';
+import RaisedButton from 'material-ui/RaisedButton';
+import {Toolbar, ToolbarGroup, ToolbarSeparator, ToolbarTitle} from 'material-ui/Toolbar';
+import Avatar from 'material-ui/Avatar';
+import Settings from 'material-ui/svg-icons/action/settings';
+
+
+
 import WidgetCardHeader from './WidgetCardHeader.jsx';
 import AudioPlayer from './AudioPlayer.jsx';
+//id generator
+import newId from '../utils/newid';
 
+//moment
+var moment = require('moment');
+
+const styles = {
+  title: {
+    cursor: 'pointer',
+    // backgroundColor: '#cccccc',
+    backgroundColor: '#333333',
+    color: '#fff'
+  },
+  date:{
+    color: '#333',
+    fontSize: '2.5em' 
+  }
+};
 
 class TimeWidget extends Component {
 
@@ -16,51 +50,118 @@ class TimeWidget extends Component {
     super(props);
     this.state = {
       expanded: false,
+      clockId: 'clock'
     };
   }
 
   handleExpandChange = (expanded) => {
       this.setState({expanded: expanded});
-    };
+  };
 
-    handleToggle = (event, toggle) => {
-      this.setState({expanded: toggle});
-    };
+  handleToggle = (event, toggle) => {
+    this.setState({expanded: toggle});
+  };
 
-    handleExpand = () => {
-      this.setState({expanded: true});
-    };
+  handleExpand = () => {
+    this.setState({expanded: true});
+  };
 
-    handleReduce = () => {
-      this.setState({expanded: false});
-    };
+  handleReduce = () => {
+    this.setState({expanded: false});
+  };
+  handleTouchTap() {
+    alert('onTouchTap triggered on the title component');
+  }
+  componentWillMount(){
+    var clockId = newId();
+    this.clockId = clockId;
+  }
 
   componentDidMount() {
-      console.log('Widget mounted');
-      var clock = $('.your-clock').FlipClock({
+  
+    var clock =$(`#${this.clockId}`).FlipClock({
         clockFace: 'TwentyFourHourClock'
-      });
-      clock.start(function() {
-        // Optional callback will fire when the clock starts
-      });
-    };
+    });       
+  };
+
+  capitalize(str) {
+    return str.charAt(0).toUpperCase() + str.slice(1);
+  }
 
    render() {
     return (
       <Card expanded={this.state.expanded} onExpandChange={this.handleExpandChange}>
-        <WidgetCardHeader widget={this.props.widget}/>
-        <div className="your-clock"></div>
+        <AppBar
+          style={styles.title}
+          iconElementLeft={
+              <div>
+                <Avatar src={WidgetIconImage[this.props.widget.widgetType]} />
+                <IconButton >
+                  <NavigationClose color='white'/>
+                </IconButton>
+                <IconButton  tooltip="Setting" touch={true} tooltipPosition="top-center">
+                  <Settings color='white'/>
+                </IconButton>
+              </div>
+          }
+          title={<span style={styles.title}>{this.capitalize(this.props.widget.widgetType)}</span>}
+          
+          iconElementRight={<FlatButton label="Save" />}
+        />
+
+        <CardHeader
+              avatar={WidgetIconImage[this.props.widget.widgetType]}
+              children={
+                <span>
+                <ToolbarSeparator />
+                <IconButton><NavigationClose /></IconButton>
+                
+                </span>
+              }
+        />
+       
+
+          
+            
+      
+        <Toolbar>
+          
+
+          <ToolbarGroup firstChild={true}>
+            <Avatar src={WidgetIconImage[this.props.widget.widgetType]} />
+            <ToolbarTitle text={this.props.widget.widgetType}/>
+          </ToolbarGroup>
+          <ToolbarGroup>
+            <ToolbarTitle text="Options" />
+            
+            <ToolbarSeparator />
+            <RaisedButton label="Create Broadcast" primary={true} />
+            <IconMenu
+              iconButtonElement={
+                <IconButton touch={true}>
+                  <NavigationExpandMoreIcon />
+                </IconButton>
+              }
+            >
+              <MenuItem primaryText="Download" />
+              <MenuItem primaryText="More Info" />
+            </IconMenu>
+          </ToolbarGroup>
+        </Toolbar>
+
+        <div className="row">
+          <div className="col s3" style={styles.date}>
+            {moment().format('dddd MMM Do')}
+          </div>
+          <div className="col s6" id={this.clockId}>
+          </div>
+        </div>
         <AudioPlayer widget={this.props.widget}/>
-  
-        <CardMedia
-          expandable={true}
-          overlay={<CardTitle title="Overlay title" subtitle="Overlay subtitle" />}
-        >
-          <img src="images/nature-600-337.jpg" />
-        </CardMedia>
+        
+      
         <CardTitle title="Card title" subtitle="Card subtitle" />
         <CardText >
-          
+            <h3>Settings</h3>
         </CardText>
         <CardActions>
           <FlatButton label="Expand" onTouchTap={this.handleExpand} />
