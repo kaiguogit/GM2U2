658fe7aa9b6d94c1e0e354e41fc9024d23979102
KennethQiu <kennethqiujunhao@gmail.com>
2016-09-06 19:17:47 -0700

    drag and drop to reorder widget array

--- frontend/src/active_playlist.jsx
+++ frontend/src/active_playlist.jsx
@@ -12,10 +12,8 @@ class ActivePlaylist extends Component {
     console.log("Did mount active playlist Playlist is", this.props.playlist);
   };
 
-  
   //Ajax call to Add widget to this playlist
   handleAddWidget(widgetType){
-
     //Post request to add widget
     $.ajax({
         url: `http://localhost:3000/api/playlists/${this.props.playlist.id}/${widgetType}Widget`,
@@ -35,25 +33,11 @@ class ActivePlaylist extends Component {
 
   //Move item in array
   //http://stackoverflow.com/questions/5306680/move-an-array-element-from-one-array-position-to-another
-
   handleMove(old_index, new_index){
-
+    console.log('widgets moved')
     var newArray = this.props.playlist.widgets
-
-    console.log("before change array", newArray);
-
     var moveItem = newArray.splice(old_index, 1)[0];
-
-    console.log("removed item, then array is", newArray);
-
     newArray.splice(new_index, 0, moveItem);
-
-
-    console.log("put item back to new index, then array is", newArray);
-
-    
-    // console.log("order changed", newArray);
-
     var newPlaylist = this.props.playlist;
     newPlaylist.widgets = newArray;
 
@@ -77,16 +61,21 @@ class ActivePlaylist extends Component {
   render() {
     return (
       <div id = 'contents' className = 'col s12 m10 offset-m1 l8 offset-l2'>
-
+        {/*
         <button onClick={this.handleAddWidget.bind(this, WidgetTypes.time)}>Add Widget</button>
         <button onClick={this.handleAddWidget.bind(this, WidgetTypes.weather)}>Add Weather</button>
         <button onClick={this.handleAddWidget.bind(this, WidgetTypes.traffic)}>Add Traffic</button>
+        */}
+
         <p>{this.props.playlist.name}</p>
         <p>Id {this.props.playlist.id}</p>
-        {
-          this.props.playlist.widgets.map(function(widget, index){
-            console.log("creating cards");
-            return <WidgetCardWrapper 
+
+        {console.log("the widget array is", this.props.playlist.widgets)}
+        
+        {this.props.playlist.widgets.map(function(widget, index){
+          console.log("creating cards", widget)
+          return( 
+            <WidgetCardWrapper 
               position={index}
               onDropWidgetIcon={this.handleAddWidget.bind(this)} 
               widget={widget} 
@@ -94,8 +83,14 @@ class ActivePlaylist extends Component {
               key={widget.id}
               onMove={this.handleMove.bind(this)}
             />
-          }.bind(this)) 
-        }
+          )
+        }.bind(this)) }
+        {/* place holder div with min height but no widget type */}
+        <WidgetCardWrapper 
+          position={this.props.playlist.widgets.length}
+          onMove={this.handleMove.bind(this)}
+          onDropWidgetIcon={this.handleAddWidget.bind(this)}
+        />
       </div>  
     );
   }
