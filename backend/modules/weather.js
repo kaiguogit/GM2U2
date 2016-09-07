cf8ad95c3d51ebd098558388d5892d5010871434
kaiguogit <git@kaiguo.ca>
2016-09-06 19:26:54 -0700

    weather widget can speak now

--- backend/modules/weather.js
+++ backend/modules/weather.js
@@ -14,26 +14,33 @@ var weather = {};
 weather.currentWeather = (function(wunderground){
   return function(city){
     return new Promise(function(resolve, reject){
+      console.log("passing city is", city);
       wunderground.conditions().forecast().request(city, function(err, response){
         if(err){
           reject(err);
         }
 
-        var current = response.current_observation;
-        var info = {
-            name: current.display_location.full,
-            icon_url: current.icon_url,
-            weather: current.weather,
-            temp_c: current.temp_c,
-        };
-        var speachInfo = {
-          name: "City is " + info.name + ". ",
-          weather: "Current weather is " + info.weather + ". ",
-          temp_c: "Temperature is " + info.temp_c + "degrees. "
+        try{
+          console.log("WU's response is ", response);
+          console.log("WU's response result is is ", response.response.results);
+          var current = response.current_observation;
+          var info = {
+              name: current.display_location.full,
+              icon_url: current.icon_url,
+              weather: current.weather,
+              temp_c: current.temp_c,
+          };
+          var speachInfo = {
+            name: "City is " + info.name + ". ",
+            weather: "Current weather is " + info.weather + ". ",
+            temp_c: "Temperature is " + info.temp_c + "degrees. "
+          }
+          var str = Object.keys(speachInfo).reduce(function(pre, cur){return pre + speachInfo[cur]}, "");            
+          resolve(str);
+        }catch(err){
+          reject(err);
         }
-        var str = Object.keys(speachInfo).reduce(function(pre, cur){return pre + speachInfo[cur]}, "");
 
-        resolve(str);
       });
     });
   }
