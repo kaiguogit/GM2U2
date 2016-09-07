38949e574ff49ca941723ec4405711dfe2952c0f
kaiguogit <git@kaiguo.ca>
2016-09-06 17:08:17 -0700

    added weather widget

--- routes/helper.js
+++ routes/helper.js
@@ -1,19 +1,20 @@
 var models = require("../models");
 
 
-function findPlaylistforWidget(widget){
+module.exports.findPlaylistforWidget = function(widget){
   console.log("\n!!!!!returned variable after create widget  is", widget.dataValues);
   return models.playlist.findById(widget.playlistId);
 }
 
-function addToPlaylistArray(playlist){
+module.exports.addToPlaylistArray = function(playlist){
   console.log("\n!!!!!returned variable after finding playlist   is", playlist.dataValues);
   console.log("\n!!!!!Binded widiget in this is", this);
   var widgets = playlist.dataValues.widgets
   widgets.push(this);
   return playlist.updateAttributes({widgets: widgets});
 }
-function deleteFromPlaylistArray(playlist, widget){
+
+module.exports.deleteFromPlaylistArray = function(playlist, widget){
   //Remove the widget from array
   console.log("\n!!!!!Deleting widget from array, id is", widget.id);
   var widgets = playlist.dataValues.widgets;
@@ -26,8 +27,14 @@ function deleteFromPlaylistArray(playlist, widget){
   return playlist.updateAttributes({widgets: widgets}).bind(this);
 }
 
-
-
-module.exports.findPlaylistforWidget = findPlaylistforWidget;
-module.exports.addToPlaylistArray = addToPlaylistArray;
-module.exports.deleteFromPlaylistArray = deleteFromPlaylistArray;
+module.exports.updatePlaylistArray = function(playlist,widget){
+  //Update the widget in array
+  console.log("\n!!!!!Updating widget in array, id is", widget.id);
+  var widgets = playlist.dataValues.widgets;
+  widgets.forEach(function(w, index){
+      if(w.id === widget.id){
+        widgets[index]= widget;
+      }
+    });
+  return playlist.updateAttributes({widgets: widgets}).bind(this);
+}
