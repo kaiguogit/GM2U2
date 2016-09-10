a7823e4471723c9388dc49eb55c84b4091ff5f72
KennethQiu <kennethqiujunhao@gmail.com>
2016-09-10 16:00:57 -0700

    before pull from kai

--- frontend/src/widgets/TrafficWidget.jsx
+++ frontend/src/widgets/TrafficWidget.jsx
@@ -86,7 +86,7 @@ class TrafficWidget extends Component {
           <iframe width="100%" height="500" frameBorder="0" style={{border:0}} allowFullScreen
             src="https://www.google.com/maps/embed/v1/directions?mode=walking&origin=place_id:ChIJVVVFhnlxhlQRVqDISA_7Lc8&destination=place_id:ChIJofc2FJt0hlQRb64ACVph3Gk&key=AIzaSyDuX8bDIG5SH98UqlVdrVyTH6K5G-pZoHY">
           </iframe>
-          
+        </CardText>          
         
       </Card>
     );
