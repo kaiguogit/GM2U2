64fec2e9dc943e9c3034ed3e3be9b1f347c194dc
kaiguogit <git@kaiguo.ca>
2016-09-10 14:04:03 -0700

    added cron job to trigger the call

--- backend/controller/playlist.js
+++ backend/controller/playlist.js
@@ -4,34 +4,50 @@ var twilioClient = require('../modules/twilio.js').client;
 var weatherController = require('./weatherWidget.js');
 var time = require('../modules/time');
 
-module.exports.ringOnAlarm = function ringOnAlarm(){
- console.log("Playlist Controller, ringONAlarm function");
+function ringOnAlarm(fn){
+ console.log("Checking all playlists alarms. Calling user if alarm is triggered.");
+ var messages = [];
   models.playlist.all({attributes: ['id', 'alarms']})
   .then(function(playlists){
     playlists.forEach(function(playlist){
-      console.log("ringOnAlarm", playlist.dataValues);
       if(anyAlarmOnTime(playlist.dataValues.alarms)){
-        ring(playlist.dataValues.id)
+
+        ring(playlist.dataValues.id, function(err, message){
+          if(err){
+            console.log("Error occurred when calling user for playlist" + playlist.dataValues.id);
+          }else{
+            console.log(`Calling user for playlist ${playlist.dataValues.id}`);
+          }
+        });
+        messages.push({message: `Playlist ${playlist.dataValues.id}'s alarm is time up. Calling User`})
+      }else{
+        console.log(`Playlist ${playlist.dataValues.id} has no alarm time up.`);
       }
     });
     
+    if (messages.length === 0){
+      fn({messsage: "No playlist has alarm triggered"});
+    }else{
+      fn(messages);
+    }
   });
 }
 
 function anyAlarmOnTime(alarms){
   var now = new Date();
-  alarms.some(function(alarm){
+  return alarms.some(function(alarm){
     var alarmTime = new Date(alarm.time);
     return alarmTime.getHours() === now.getHours() && alarmTime.getMinutes() === now.getMinutes();
   })
 }
-module.exports.ring = function ring(playlistId, fn){
+
+function ring(playlistId, fn){
    // This should be the publicly accessible URL for your application
   // Here, we just use the host for the application making the request,
   // but you can hard code it or use something different if need be
   // var url = 'http://' + request.headers.host + '/outbound';
   // var url = 'https://gmtestdesploy.herokuapp.com/outbound';
-  var url = "http://b6992d66.ngrok.io/outbound?playlistId=" + playlistId;
+  var url =  process.env.proxy + "/outbound?playlistId=" + playlistId;
   
   models.playlist.findById(playlistId)
   .then(function(playlist){
@@ -52,7 +68,7 @@ module.exports.ring = function ring(playlistId, fn){
   });
 }
 
-module.exports.twilioSpeech = function twilioSpeech(playlistId, fn){
+function twilioSpeech(playlistId, fn){
 
   console.log("In PlaylistController, call method, playlistId is", playlistId);
   models.playlist.findById(playlistId)
@@ -89,7 +105,7 @@ module.exports.twilioSpeech = function twilioSpeech(playlistId, fn){
      console.log("waiting on promise all to finish");
       speeches.forEach(function(speech){
         
-        var downloadURL = 'http://b6992d66.ngrok.io/api/synthesize' +
+        var downloadURL = process.env.proxy + '/api/synthesize' +
           '?voice=' + 'en-US_AllisonVoice' +
           '&accept=' + 'audio/wav' +
           '&text=' + encodeURIComponent(speech) +
@@ -109,3 +125,9 @@ module.exports.twilioSpeech = function twilioSpeech(playlistId, fn){
     });
   });
 }
+var playlistController = {
+  twilioSpeech: twilioSpeech,
+  ring: ring,
+  ringOnAlarm: ringOnAlarm
+};
+module.exports = playlistController
