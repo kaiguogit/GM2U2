5bc5e9e52d5395239ac99d3a8f62e65d63e80cf5
kaiguogit <git@kaiguo.ca>
2016-09-13 14:20:51 -0700

    fixed dependency for production

---
+++ frontend/server.bundle.js
@@ -0,0 +1,162 @@
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId])
+/******/ 			return installedModules[moduleId].exports;
+
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			exports: {},
+/******/ 			id: moduleId,
+/******/ 			loaded: false
+/******/ 		};
+
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+
+/******/ 		// Flag the module as loaded
+/******/ 		module.loaded = true;
+
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+
+
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(0);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/***/ function(module, exports, __webpack_require__) {
+
+	/* WEBPACK VAR INJECTION */(function(__dirname) {'use strict';
+
+	var _react = __webpack_require__(1);
+
+	var _react2 = _interopRequireDefault(_react);
+
+	var _server = __webpack_require__(2);
+
+	var _reactRouter = __webpack_require__(3);
+
+	var _routes = __webpack_require__(4);
+
+	var _routes2 = _interopRequireDefault(_routes);
+
+	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+	var express = __webpack_require__(6);
+	var path = __webpack_require__(7);
+	var compression = __webpack_require__(8);
+
+	var app = express();
+	// import some new stuff
+
+	// we'll use this to render our app to an html string
+
+	// and these to match the url to routes and then render
+
+
+	app.use(compression());
+
+	// serve our static stuff like index.css
+	app.use(express.static(path.join(__dirname, 'public')));
+
+	// send all requests to index.html so browserHistory works
+
+	app.get('*', function (req, res) {
+	  // match the routes to the url
+	  (0, _reactRouter.match)({ routes: _routes2.default, location: req.url }, function (err, redirect, props) {
+	    // `RouterContext` is the what `Router` renders. `Router` keeps these
+	    // `props` in its state as it listens to `browserHistory`. But on the
+	    // server our app is stateless, so we need to use `match` to
+	    // get these props before rendering.
+	    var appHtml = (0, _server.renderToString)(_react2.default.createElement(_reactRouter.RouterContext, props));
+
+	    // dump the HTML into a template, lots of ways to do this, but none are
+	    // really influenced by React Router, so we're just using a little
+	    // function, `renderPage`
+	    res.render("./public/index.html");
+	  });
+	});
+
+	var PORT = process.env.PORT || 8080;
+	app.listen(PORT, function () {
+	  console.log('Production Express server running at localhost:' + PORT);
+	});
+	/* WEBPACK VAR INJECTION */}.call(exports, ""))
+
+/***/ },
+/* 1 */
+/***/ function(module, exports) {
+
+	module.exports = require("react");
+
+/***/ },
+/* 2 */
+/***/ function(module, exports) {
+
+	module.exports = require("react-dom/server");
+
+/***/ },
+/* 3 */
+/***/ function(module, exports) {
+
+	module.exports = require("react-router");
+
+/***/ },
+/* 4 */
+/***/ function(module, exports, __webpack_require__) {
+
+	'use strict';
+
+	var _react = __webpack_require__(1);
+
+	var _react2 = _interopRequireDefault(_react);
+
+	var _reactRouter = __webpack_require__(3);
+
+	var _App = __webpack_require__(!(function webpackMissingModule() { var e = new Error("Cannot find module \"./App.jsx\""); e.code = 'MODULE_NOT_FOUND'; throw e; }()));
+
+	var _App2 = _interopRequireDefault(_App);
+
+	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+	module.exports = _react2.default.createElement(_reactRouter.Route, { path: '/', component: _App2.default });
+
+/***/ },
+/* 5 */,
+/* 6 */
+/***/ function(module, exports) {
+
+	module.exports = require("express");
+
+/***/ },
+/* 7 */
+/***/ function(module, exports) {
+
+	module.exports = require("path");
+
+/***/ },
+/* 8 */
+/***/ function(module, exports) {
+
+	module.exports = require("compression");
+
+/***/ }
+/******/ ]);
