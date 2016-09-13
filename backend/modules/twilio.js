a75556d19e4704e86f151fec0a6cc9cf1f5400a1
kaiguogit <git@kaiguo.ca>
2016-09-13 13:59:36 -0700

    finished two servers

--- backend/modules/twilio.js
+++
@@ -1,9 +0,0 @@
-
-// Twilio
-var twilio = require('twilio');
-// Create a Twilio REST API client for authenticated requests to Twilio
-var client = twilio(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN);
-
-
-module.exports.client = client;
-module.exports.twilio = twilio;
