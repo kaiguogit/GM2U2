bb4eb57e693d2c3d2270308842e8692eff71ec84
kaiguogit <git@kaiguo.ca>
2016-09-13 15:07:51 -0700

    change package json to deploy to heroku

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 var webpack = require('webpack')
 var path = require('path');
+
 if(process.env.NODE_ENV !== 'production'){
   require('dotenv').config();
   console.log("host in env is", process.env.host);
